3cfe8984c56ad29e4008043a0a023daacd19acaf
unknown <matgus@MATTIAS-NB.cad-q.net>
2015-12-26 18:45:49 +0100

    plonk

---
+++ 2dBlob/Assets/StartLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel : MonoBehaviour {
+
+	public int nextLevel = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Application.loadedLevel("test");
+	}
+
+	void nextLevel () {
+		Application.loadedLevel("test");
+	}
+}
